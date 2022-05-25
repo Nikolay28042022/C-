e09674aca7b29dbc844e4e2a08512cbdff46e582
Nikolay <vulkank@mail.ru>
2022-05-25 19:13:13 +0300

    Четвертый проект. Деление чисел

---
+++ Examples004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 20;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+double n1 = 12;
+double n2 = 5;
+Console.Write(n1 / n2);
